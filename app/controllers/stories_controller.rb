1c7ad3fb7eb16467182613f547b3e03542ca3d29
hsnuwyt <hsnuwyt@gmail.com>
2021-03-04 11:51:16 +0800

    old

---
+++ app/controllers/stories_controller.rb
@@ -0,0 +1,65 @@
+class StoriesController < ApplicationController
+    before_action :authenticate_user!
+    before_action :find_story, only: [:edit,:destroy,:update]
+
+
+    def index
+        @stories = current_user.stories.order(updated_at: :desc) 
+
+    end
+    def new
+        @story = current_user.stories.new
+    end
+    def create
+      @story = current_user.stories.new(story_params)
+      # @story.status = 'published' if params[:publish]
+      @story.publish! if params[:publish]
+  
+      if @story.save
+        if params[:publish]
+          redirect_to stories_path, notice: '已成功發佈故事'
+        else
+          redirect_to edit_story_path(@story), notice: '故事已儲存'
+        end
+      else
+        render :new
+      end
+    end
+
+    
+    def edit
+    
+
+    end
+    def destroy
+        @story.destroy
+        redirect_to stories_path,notice: '故事已刪除'   
+
+    end
+    def update
+        if @story.update(story_params)
+          case
+          when params[:publish]
+            @story.publish!
+            redirect_to stories_path, notice: '故事已發佈'
+          when params[:unpublish]
+            @story.unpublish!
+            redirect_to stories_path, notice: '故事已下架'
+          else
+            redirect_to edit_story_path(@story), notice: '故事已儲存'
+          end
+        else
+          render :edit
+        end
+      end
+
+
+
+    private
+    def find_story
+      @story = current_user.stories.friendly.find(params[:id])
+    end
+    def story_params
+        params.require(:story).permit(:title,:content)
+    end
+end
---
+++ app/models/story.rb
@@ -0,0 +1,41 @@
+class Story < ApplicationRecord
+  include AASM
+  extend FriendlyId
+  friendly_id :slug_candidate, use: :slugged
+
+
+  belongs_to :user
+  validates :title,presence: true
+
+  default_scope{where( deleted_at: nil)}
+
+  def destroy
+    update(deleted_at: Time.now)
+    
+  end
+  def normalize_friendly_id(input)
+    input.to_s.to_slug.normalize(transliterations: :russian).to_s
+  end
+
+  aasm(column: 'status', no_direct_assignment: true) do
+    state :draft, initial: true
+    state :published
+
+    event :publish do
+      transitions from: :draft, to: :published
+    end
+
+    event :unpublish do
+      transitions from: :published, to: :draft
+    end
+  end
+  private
+
+  def slug_candidate
+    [
+      :title,
+      [:title, SecureRandom.hex[0, 8]]
+    ]
+  end
+
+end
